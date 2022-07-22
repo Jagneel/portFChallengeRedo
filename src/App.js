988ec8cb9c83889e8e22e25381a0720747f21b42
Jagneel <jagneel@hotmail.com>
2022-07-22 15:00:57 +0100

    project finally working

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component, useEffect, useState } from 'react';
 import './App.css';
 import { AbvFilter, Chart, DatePicker } from './components';
 import punkapi from './api/punkApi'
-import axios from 'axios';
 
 const App = () => {
     
@@ -15,7 +14,6 @@ const App = () => {
     const [yearTill, monthTill, dayTill] = dateTill.split('-');
     const newDateTill = `${monthTill}-${yearTill}`
 
-
     // Create function that filters beerData by ABV 
 
     const [abvLow, setAbvLow] = useState(0)
@@ -25,7 +23,9 @@ const App = () => {
     // Use initial set date and abv data to input params into url api and render a chart on page load
 
     const [beerData, setBeerData ] = useState([])
-
+    const [isData, setisData] = useState(false)
+    
+    
     const fetchData = async () =>  {
       const response = await punkapi.get('beers', {
         params: {
@@ -37,7 +37,9 @@ const App = () => {
         }
       })
       setBeerData(response.data)
+      setisData(true)
     }
+
 
     // reformat the beerData into a modified data for chart
 
@@ -66,31 +68,35 @@ const App = () => {
         } 
      }
 
+     // Sort in ascending order
+
      dataSlice.sort(
       (a, b) => (a.year - b.year) || (a.month - b.month)
      );
    
-    let count = {}
+    let count = []
+
+    // count the data so the Nivo chart can read and output our dataset
 
     dataSlice.forEach(function(i){
         count[i.monthYear] = (count[i.monthYear] || 0 ) + 1;
     })
 
-    var countObj = Object.entries(count)
-
-     console.log(countObj)
-
     
 
+    var countObj = Object.entries(count)
+
+    console.log(countObj)
+
+   
  
     return (
       <div className="App">
         <DatePicker dateFrom={dateFrom} setDateFrom={setDateFrom} dateTill={dateTill} setDateTill={setDateTill} onChange={() => {}} />
         <AbvFilter setAbvLow={setAbvLow} setAbvHigh={setAbvHigh}/><br></br>
-        <button onClick={fetchData}>Get Data</button>
-        <Chart data={countObj}/>
-        
-        
+        <button onClick={fetchData}>Render Table</button>
+        {/* <Chart  data={countObj}/> */}
+        { isData ? <Chart data={countObj}/> : <div><br></br>Loading...</div>}
       </div>
     )
 }
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -3,36 +3,43 @@ import { ResponsiveBar } from '@nivo/bar';
 
 const Chart = (data) => {
 
-    
+    console.log(data.data)
 
-    const barChart = (
-        <ResponsiveBar
-        data={data}
-        indexBy="first_brewed"
-        keys={['abv']}
-        margin={{ top: 50, right: 130, bottom: 100, left: 160 }}
-        padding={0.4}
-        valueScale={{ type: "linear" }}
-        colors="#FFAC1C"
-        animate={true}
-        enableLabel={false}
-        axisTop={null}
-        axisRight={null}
-        enableLabel={true}
-        axisLeft={{
-          tickSize: 5,
-          tickPadding: 5,
-          tickRotation: 0,
-          legend: "Sum of beers first brewed",
-          legendPosition: "middle",
-          legendOffset: -40
-        }}
-        axisBottom={null}
-      />
-    ) 
+        const barChart = (
+            <ResponsiveBar
+                data={data.data}
+                indexBy="0"
+                keys={['1']}
+                margin={{ top: 50, right: 130, bottom: 100, left: 160 }}
+                padding={0.4}
+                valueScale={{ type: "linear" }}
+                colors="#FFAC1C"
+                animate={true}
+                enableLabel={false}
+                axisTop={null}
+                axisRight={null}
+                enableLabel={true}
+                axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Sum of beers first brewed",
+                legendPosition: "middle",
+                legendOffset: -40
+                }}
+                axisBottom={{
+                    legend: "0"
+                }}
+            />
+        ) 
 
     return (
-        <div className="table" style={{ height: "800px" }}></div>
+        
+        <div className="table" style={{ height: "800px" }}>
+        {barChart} 
+        </div>
+               
+        
     )
 }
 
